6c61f1bb593bfbe933a47ea6b27e4db533d0de36
Fer Nachbauer <67807496+fernachbauer@users.noreply.github.com>
2025-01-22 22:54:35 -0300

    Update api_stress.py

--- tests/stress/api_stress.py
+++ tests/stress/api_stress.py
@@ -9,14 +9,20 @@ class StressUser(HttpUser):
             json={
                 "flights": [
                     {
-                        "OPERA": "Aerolineas Argentinas", 
-                        "TIPOVUELO": "N", 
-                        "MES": 3
+                        "OPERA": "Aerolineas Argentinas",
+                        "TIPOVUELO": "N",
+                        "MES": 3,
+                        "Fecha-I": "2025-01-01 00:00:00",
+                        "Fecha-O": "2025-01-01 02:00:00",
+                        "SIGLAORI": "SCL",
+                        "SIGLADES": "EZE",
+                        "DIANOM": "Lunes",
+                        "Vlo-I": "AR1234",
+                        "Emp-I": "AR"
                     }
                 ]
             }
         )
-
 
     @task
     def predict_latam(self):
@@ -25,9 +31,16 @@ class StressUser(HttpUser):
             json={
                 "flights": [
                     {
-                        "OPERA": "Grupo LATAM", 
-                        "TIPOVUELO": "N", 
-                        "MES": 3
+                        "OPERA": "Grupo LATAM",
+                        "TIPOVUELO": "I",
+                        "MES": 7,
+                        "Fecha-I": "2025-06-15 12:30:00",
+                        "Fecha-O": "2025-06-15 14:00:00",
+                        "SIGLAORI": "LIM",
+                        "SIGLADES": "BOG",
+                        "DIANOM": "Martes",
+                        "Vlo-I": "LA5678",
+                        "Emp-I": "LA"
                     }
                 ]
             }
